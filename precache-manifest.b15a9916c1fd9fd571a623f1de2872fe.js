d9b21af499e8c129786255e190460d406b465132
Joan K Njoroge <joan.njoroge0@gmail.com>
2020-01-06 09:53:32 +0300

    Updates

---
+++ precache-manifest.b15a9916c1fd9fd571a623f1de2872fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa0e28dc868b4acfe1c97d64417f94a0",
+    "url": "/portfolio-repo/index.html"
+  },
+  {
+    "revision": "922bfd78a55402e93d31",
+    "url": "/portfolio-repo/static/css/2.559fd73f.chunk.css"
+  },
+  {
+    "revision": "1fbec68c2e809862c408",
+    "url": "/portfolio-repo/static/css/main.ac7809c1.chunk.css"
+  },
+  {
+    "revision": "922bfd78a55402e93d31",
+    "url": "/portfolio-repo/static/js/2.1c1d006b.chunk.js"
+  },
+  {
+    "revision": "369c35024efebe5c40542bf0ba67a549",
+    "url": "/portfolio-repo/static/js/2.1c1d006b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1fbec68c2e809862c408",
+    "url": "/portfolio-repo/static/js/main.62f57cb8.chunk.js"
+  },
+  {
+    "revision": "feae7dd5f16620ed8092",
+    "url": "/portfolio-repo/static/js/runtime-main.26944a9c.js"
+  }
+]);
